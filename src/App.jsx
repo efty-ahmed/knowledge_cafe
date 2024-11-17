07c1c1a9d30f48e4746aa4e9a7c823a255259526
efty-ahmed <eftyahmed1190@gmail.com>
2024-11-17 15:20:01 +0600

    blogs work and proptype work done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Blogs from './component/Blogs/Blogs'
+import Bookmarks from './component/Bookmarks/Bookmarks'
 import Header from './component/Header/Header'
 
 function App() {
@@ -8,7 +9,10 @@ function App() {
   return (
     <>
      <Header></Header>
+     <div className='md:flex'>
      <Blogs></Blogs>
+     <Bookmarks></Bookmarks>
+     </div>
       
     </>
   )
---
+++ src/component/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,11 @@
+
+
+const Bookmarks = () => {
+    return (
+        <div className="md:h-1/3">
+           <h1>Bookmarks</h1> 
+        </div>
+    );
+};
+
+export default Bookmarks;
